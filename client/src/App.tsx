16ea9b226410a30839241eee09e2ab2fed455985
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-20 14:54:22 -0300

    chore: husky test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
-  return (
-    <main className="m-12">
-      <Typography variant="h5" color="textSecondary" align="center">
-        Order Manager
-      </Typography>
-    </main>
-  );
+    return (
+        <main className="m-12">
+            <Typography variant="h5" color="textSecondary" align="center">
+                Order Manager
+            </Typography>
+        </main>
+    );
 }
 
 export default App;
