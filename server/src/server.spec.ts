92bbae9e4335cf4e37de89969b0fb1675dfd256b
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-20 10:32:58 -0300

    chore: test husky

--- server/src/server.spec.ts
+++ server/src/server.spec.ts
@@ -1,5 +1,5 @@
-describe("Test propose", () => {
-  it("returns 4", () => {
-    expect(2 + 2).toBe(4);
+describe('Test propose', () => {
+  it('returns 6', () => {
+    expect(3 + 3).toBe(6);
   });
 });
