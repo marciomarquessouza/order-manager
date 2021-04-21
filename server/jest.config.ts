d9c5b0f2b45c846fd85df5e70040e4d7c1f1f0da
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-21 00:26:18 -0300

    chore: add module alias config to jest and typescript

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,10 +1,19 @@
 export default {
-  clearMocks: true,
-  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
-  roots: ["<rootDir>"],
-  testEnvironment: "node",
-  testRegex: "./src/.*\\.(test|spec)?\\.(ts|ts)$",
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
+    clearMocks: true,
+    moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+    roots: ['<rootDir>'],
+    testEnvironment: 'node',
+    testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
+    transform: {
+        '^.+\\.ts?$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1',
+        '@entities/(.*)': '<rootDir>/src/entities/$1',
+        '@repositories/(.*)': '<rootDir>/src/repositories/$1',
+        '@presenters/(.*)': '<rootDir>/src/presenters/$1',
+        '@infra/(.*)': '<rootDir>/src/infra/$1',
+        '@usecases/(.*)': '<rootDir>/src/usecases/$1',
+        '@main/(.*)': '<rootDir>/src/main/$1',
+    },
 };
