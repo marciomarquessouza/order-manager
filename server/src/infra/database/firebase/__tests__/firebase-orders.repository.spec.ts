2f1658960196306454db848b220d344527055164
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-21 17:55:29 -0300

    test: ensure that firebase set method was called with correct parameters

---
+++ server/src/infra/database/firebase/__tests__/firebase-orders.repository.spec.ts
@@ -0,0 +1,32 @@
+import { mockCreateOrderParams } from '@/usecases/create-order/mock/mock-create-order.usecase';
+import { FirebaseOrderRepository } from '../firebase-orders.repository';
+
+const setSpy = jest.fn();
+
+jest.mock('../firebase-helper', () => ({
+    FirebaseHelper: {
+        initialize: jest.fn(),
+        getCollection: () => ({
+            doc: () => ({
+                set: setSpy,
+            }),
+        }),
+    },
+}));
+
+const makeSut = (): FirebaseOrderRepository => {
+    return new FirebaseOrderRepository();
+};
+
+describe('#Firebase Database | Create Order', () => {
+    describe('create()', () => {
+        describe('when new order is created', () => {
+            it('calls the set command from firebase with correct parameters', async () => {
+                const sut = makeSut();
+                const createOrderParams = mockCreateOrderParams();
+                await sut.create(createOrderParams);
+                expect(setSpy).toHaveBeenCalledWith(createOrderParams);
+            });
+        });
+    });
+});
