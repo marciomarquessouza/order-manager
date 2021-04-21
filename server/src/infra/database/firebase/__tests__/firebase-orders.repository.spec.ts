55e3e339fdf48fb1d0c974bf1dc53d29059fe72b
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-21 18:29:26 -0300

    fix: add parameter id

--- server/src/infra/database/firebase/__tests__/firebase-orders.repository.spec.ts
+++ server/src/infra/database/firebase/__tests__/firebase-orders.repository.spec.ts
@@ -1,5 +1,6 @@
 import { mockCreateOrderParams } from '@/usecases/create-order/mock/mock-create-order.usecase';
 import { FirebaseOrderRepository } from '../firebase-orders.repository';
+import { uuid } from 'uuidv4';
 
 const setSpy = jest.fn();
 
@@ -18,14 +19,16 @@ const makeSut = (): FirebaseOrderRepository => {
     return new FirebaseOrderRepository();
 };
 
+const uid = uuid();
+
 describe('#Firebase Database | Create Order', () => {
     describe('create()', () => {
         describe('when new order is created', () => {
             it('calls the set command from firebase with correct parameters', async () => {
                 const sut = makeSut();
-                const createOrderParams = mockCreateOrderParams();
-                await sut.create(createOrderParams);
-                expect(setSpy).toHaveBeenCalledWith(createOrderParams);
+                const parameter = { uid, ...mockCreateOrderParams() };
+                await sut.create(parameter);
+                expect(setSpy).toHaveBeenCalledWith(parameter);
             });
         });
     });
