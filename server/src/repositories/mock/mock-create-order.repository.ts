38d9d53a2bb6b7292e73e07d741e92df9c22f680
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-21 11:56:41 -0300

    refactor: replace mock order data by mockOrder function

--- server/src/repositories/mock/mock-create-order.repository.ts
+++ server/src/repositories/mock/mock-create-order.repository.ts
@@ -1,25 +1,10 @@
+import { mockOrder } from '@/entities/mocks/mock-order';
 import { CreateOrderRepository } from '@repositories/create-order.repository';
 import { CreateOrder } from '@usecases/create-order/create-order.protocols';
-import faker from 'faker';
 
 export class CreateOrderRepositorySpy implements CreateOrderRepository {
     public params: CreateOrderRepository.Params;
-    result: CreateOrder.Result = {
-        id: faker.datatype.uuid(),
-        title: faker.commerce.product(),
-        bookingDate: faker.date.future(),
-        customer: {
-            name: faker.name.firstName(),
-            email: faker.internet.email(),
-            phone: faker.phone.phoneNumber(),
-        },
-        address: {
-            street: faker.address.streetAddress(),
-            city: faker.address.city(),
-            country: faker.address.country(),
-            zip: faker.address.zipCode(),
-        },
-    };
+    result: CreateOrder.Result = mockOrder();
 
     async create(data: CreateOrder.Params): Promise<CreateOrder.Result> {
         this.params = data;
