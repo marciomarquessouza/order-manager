73b719fb91eaf7289b88c7946cc453073a97e0b3
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-21 10:53:42 -0300

    refactor: change the result attribute

--- server/src/repositories/mock/mock-update-order.repository.ts
+++ server/src/repositories/mock/mock-update-order.repository.ts
@@ -4,12 +4,11 @@ import { UpdateOrder } from '@/usecases/update-order/update-order.protocols';
 export class UpdateOrderRepositorySpy implements UpdateOrderRepository {
     id: string;
     params: UpdateOrderRepository.Params;
-    result: UpdateOrderRepository.Result;
+    result: UpdateOrderRepository.Result = true;
 
     async update(data: UpdateOrder.Params, id: string): Promise<UpdateOrder.Result> {
         this.id = id;
         this.params = data;
-        this.result = true;
         return this.result;
     }
 }
