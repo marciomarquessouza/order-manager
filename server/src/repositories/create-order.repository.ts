cb8a47a7f1970a2c0e0fdc89d52d51386b063d7e
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-21 17:05:09 -0300

    chore: changing the create return to void to follow the command query separation principle

--- server/src/repositories/create-order.repository.ts
+++ server/src/repositories/create-order.repository.ts
@@ -2,9 +2,8 @@ import { CreateOrder } from '@usecases/create-order/create-order.protocols';
 
 export namespace CreateOrderRepository {
     export type Params = CreateOrder.Params;
-    export type Result = CreateOrder.Result;
 }
 
 export interface CreateOrderRepository {
-    create(data: CreateOrderRepository.Params): Promise<CreateOrderRepository.Result>;
+    create(data: CreateOrderRepository.Params): Promise<void>;
 }
--- server/src/repositories/mock/mock-create-order.repository.ts
+++ server/src/repositories/mock/mock-create-order.repository.ts
@@ -1,13 +1,10 @@
-import { mockOrder } from '@/entities/mocks/mock-order';
 import { CreateOrderRepository } from '@repositories/create-order.repository';
 import { CreateOrder } from '@usecases/create-order/create-order.protocols';
 
 export class CreateOrderRepositorySpy implements CreateOrderRepository {
     public params: CreateOrderRepository.Params;
-    result: CreateOrder.Result = mockOrder();
 
-    async create(data: CreateOrder.Params): Promise<CreateOrder.Result> {
+    async create(data: CreateOrder.Params): Promise<void> {
         this.params = data;
-        return this.result;
     }
 }
