332142974537e1d8d82fc5f5b6dbeb6feec1a886
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-22 15:32:03 -0300

    chore: updating middleware index

--- server/src/main/middlewares/index.ts
+++ server/src/main/middlewares/index.ts
@@ -1,3 +1,6 @@
 export * from './auth-by-token-middleware';
 export * from './middleware.adapter';
 export * from './middleware.protocol';
+export * from './body-parser';
+export * from './content-type';
+export * from './cors';
