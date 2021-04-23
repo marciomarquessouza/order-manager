76e3c4df4c6f41854ec69aef0e82cec3ca0f0a8d
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 18:37:23 -0300

    refactor: update load devToken return to string

--- server/src/infra/auth/firebase/firebase-load-devtoken.ts
+++ server/src/infra/auth/firebase/firebase-load-devtoken.ts
@@ -3,8 +3,7 @@ import { LoadDevToken } from '@/usecases/load-devtoken/load-dev-token.protocols'
 import { FirebaseAuthHelper } from './firebase-helper';
 
 export class FirebaseLoadDevToken implements LoadDevTokenRepository {
-    async load(params: LoadDevToken.Params): Promise<LoadDevToken.Result> {
-        const devToken = await FirebaseAuthHelper.getDevToken(params.userId);
-        return { devToken };
+    async load(params: LoadDevToken.Params): Promise<LoadDevTokenRepository.Result> {
+        return await FirebaseAuthHelper.getDevToken(params.userId);
     }
 }
