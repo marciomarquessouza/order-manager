6eac958cd921de4a98dd1a4c00397b36c898d27c
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 08:30:42 -0300

    feat: add load orders controller factory

--- server/src/main/factories/index.ts
+++ server/src/main/factories/index.ts
@@ -1,3 +1,4 @@
 export * from './auth-by-token-middleware.factory';
 export * from './create-order-controller.factory';
 export * from './update-order-controller.factory';
+export * from './load-orders-controller.factory';
