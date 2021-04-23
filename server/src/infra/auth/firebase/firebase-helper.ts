beb4fbe41e90533cbb97067b2b1aa876df29631a
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 14:12:11 -0300

    feat: add get IdToken in FirebaseAuthHelper

--- server/src/infra/auth/firebase/firebase-helper.ts
+++ server/src/infra/auth/firebase/firebase-helper.ts
@@ -1,7 +1,28 @@
 import firebase from 'firebase-admin';
+import fetch from 'node-fetch';
 
-export const FirebaseHelper = {
+export const FirebaseAuthHelper = {
     async verifyIdToken(token: string) {
         return await firebase.auth().verifyIdToken(token);
+    },
+    async getTokenId(uid: string): Promise<string> {
+        try {
+            const customToken = await firebase.auth().createCustomToken(uid);
+            const response = await fetch(
+                `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyDcMG3Wr5rB8n8hcGvh7kbFisvSx_k4uxk`,
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        token: customToken,
+                        returnSecureToken: true,
+                    }),
+                },
+            );
+            const content = await response.json();
+            return content.idToken;
+        } catch (error) {
+            console.log('content', error.message);
+            return `No token today ${error.message}`;
+        }
     },
 };
