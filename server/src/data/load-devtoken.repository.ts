06e08bd609fe7438b075e777049b3286d0407322
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 18:37:03 -0300

    refactor: update load devToken return to string

--- server/src/data/load-devtoken.repository.ts
+++ server/src/data/load-devtoken.repository.ts
@@ -2,7 +2,7 @@ import { LoadDevToken } from '@/usecases/load-devtoken/load-dev-token.protocols'
 
 export namespace LoadDevTokenRepository {
     export type Params = LoadDevToken.Params;
-    export type Result = LoadDevToken.Result;
+    export type Result = string;
 }
 
 export interface LoadDevTokenRepository {
