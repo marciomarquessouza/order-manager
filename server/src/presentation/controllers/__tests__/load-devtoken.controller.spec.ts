2143c28a0abc429f8dbf0d0e713bfdbf8d4c2968
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 19:19:35 -0300

    test: ensure that it will return 401 case the environment is different of  DEV

--- server/src/presentation/controllers/__tests__/load-devtoken.controller.spec.ts
+++ server/src/presentation/controllers/__tests__/load-devtoken.controller.spec.ts
@@ -1,28 +1,32 @@
 import { LoadDevTokenSpy } from '@/usecases/load-devtoken/mock/mock-load-dev-token.usecase';
 import { LoadDevTokenController } from '../load-devtoken.controller';
 import faker from 'faker';
+import { env } from '@/main/config';
+import { LoadDevTokenRepositorySpy } from '@/data/mock/mock-load-devtoken.repository';
+import { LoadDevTokenUseCase } from '@/usecases/load-devtoken/load-dev-token.usecase';
 
 type SutTypes = {
     sut: LoadDevTokenController;
-    loadDevTokenSpy: LoadDevTokenSpy;
+    loadDevTokenRepositorySpy: LoadDevTokenRepositorySpy;
 };
 
 const makeSut = (): SutTypes => {
-    const loadDevTokenSpy = new LoadDevTokenSpy();
-    const sut = new LoadDevTokenController(loadDevTokenSpy);
+    const loadDevTokenRepositorySpy = new LoadDevTokenRepositorySpy();
+    const loadDevTokenUseCase = new LoadDevTokenUseCase(loadDevTokenRepositorySpy);
+    const sut = new LoadDevTokenController(loadDevTokenUseCase);
     return {
         sut,
-        loadDevTokenSpy,
+        loadDevTokenRepositorySpy,
     };
 };
 
 describe('#Controller | Load DevToken', () => {
     describe('when the flow to load DevToken is called', () => {
         it('calls the load DevToken Use Case', async () => {
-            const { sut, loadDevTokenSpy } = makeSut();
+            const { sut, loadDevTokenRepositorySpy } = makeSut();
             const params = { userId: faker.datatype.uuid() };
             await sut.handle(params);
-            expect(loadDevTokenSpy.params).toBe(params);
+            expect(loadDevTokenRepositorySpy.params).toBe(params);
         });
 
         it('returns 200 on success', async () => {
@@ -32,19 +36,21 @@ describe('#Controller | Load DevToken', () => {
             expect(result.statusCode).toBe(200);
         });
 
-        it.skip('returns 204 in case of no contents', async () => {
-            const { sut, loadOrdersSpy } = makeSut();
-            loadOrdersSpy.result = [];
-            const result = await sut.handle();
-            expect(result.statusCode).toBe(204);
+        it('returns 401 when the environment is not DEV', async () => {
+            const { sut } = makeSut();
+            env.app_env = 'PROD';
+            const params = { userId: faker.datatype.uuid() };
+            const result = await sut.handle(params);
+            expect(result.statusCode).toBe(401);
         });
 
         it.skip('returns 500 if thow an error ', async () => {
-            const { sut, loadOrdersSpy } = makeSut();
-            jest.spyOn(loadOrdersSpy, 'execute').mockImplementationOnce(() => {
+            const { sut, loadDevTokenRepositorySpy } = makeSut();
+            jest.spyOn(loadDevTokenRepositorySpy, 'load').mockImplementationOnce(() => {
                 throw new Error();
             });
-            const result = await sut.handle();
+            const params = { userId: faker.datatype.uuid() };
+            const result = await sut.handle(params);
             expect(result.statusCode).toBe(500);
         });
     });
