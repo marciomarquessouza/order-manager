f61e2225bff4a7fb5694c8eaf54ba05a75d4ceae
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 17:36:28 -0300

    refactor: replacing dot env by env file in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,10 @@
-import { app } from '@main/config';
+import { app, env } from '@main/config';
 import { FirebaseHelper } from '@infra/database';
-import { config } from 'dotenv';
 
 const firebaseServiceAccount = require('../serviceAccount.json');
 
-config();
-
 FirebaseHelper.initialize(firebaseServiceAccount);
 
-const PORT = process.env.PORT || 5001;
+const PORT = env.port || 5001;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
