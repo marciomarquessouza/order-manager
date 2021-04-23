2ab83bac45968015b75c6c365f7a176cc5b911f2
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 18:38:46 -0300

    feat: return unauthorized error if the devToken is called in environment different of DEV

--- server/src/usecases/load-devtoken/load-dev-token.usecase.ts
+++ server/src/usecases/load-devtoken/load-dev-token.usecase.ts
@@ -1,10 +1,16 @@
 import { LoadDevTokenRepository } from '@/data/load-devtoken.repository';
 import { LoadDevToken } from './load-dev-token.protocols';
+import { env } from '@/main/config';
+import { UnauthorizedError } from '@/presentation/errors';
 
 export class LoadDevTokenUseCase implements LoadDevToken {
     constructor(private readonly loadDevTokenRepository: LoadDevTokenRepository) {}
 
     async execute(data: LoadDevToken.Params): Promise<LoadDevToken.Result> {
-        return this.loadDevTokenRepository.load(data);
+        if (env.app_env !== 'DEV') {
+            throw new UnauthorizedError();
+        }
+        const devToken = await this.loadDevTokenRepository.load(data);
+        return { devToken };
     }
 }
