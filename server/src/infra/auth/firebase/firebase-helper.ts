765ee57e6c8facfa6dd8f83b03fccc9b81c9aff2
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-23 18:06:07 -0300

    refactor: removing useless try catch and using data from contants and env file

--- server/src/infra/auth/firebase/firebase-helper.ts
+++ server/src/infra/auth/firebase/firebase-helper.ts
@@ -1,3 +1,4 @@
+import { env } from '@/main/config';
 import firebase from 'firebase-admin';
 import fetch from 'node-fetch';
 
@@ -5,24 +6,16 @@ export const FirebaseAuthHelper = {
     async verifyIdToken(token: string) {
         return await firebase.auth().verifyIdToken(token);
     },
-    async getTokenId(uid: string): Promise<string> {
-        try {
-            const customToken = await firebase.auth().createCustomToken(uid);
-            const response = await fetch(
-                `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyDcMG3Wr5rB8n8hcGvh7kbFisvSx_k4uxk`,
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        token: customToken,
-                        returnSecureToken: true,
-                    }),
-                },
-            );
-            const content = await response.json();
-            return content.idToken;
-        } catch (error) {
-            console.log('content', error.message);
-            return `No token today ${error.message}`;
-        }
+    async getDevToken(uid: string): Promise<string> {
+        const customToken = await firebase.auth().createCustomToken(uid);
+        const response = await fetch(`${FIREBASE_TOKEN_ID_API}?key=${env.api_key}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                token: customToken,
+                returnSecureToken: true,
+            }),
+        });
+        const content = await response.json();
+        return content.idToken;
     },
 };
