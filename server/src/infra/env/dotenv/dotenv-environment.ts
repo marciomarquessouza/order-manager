b98fadeab1e74dcca6e1dc1c2200493f97b106d1
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-24 09:18:20 -0300

    refactor: update list environments implementation

--- server/src/infra/env/dotenv/dotenv-environment.ts
+++ server/src/infra/env/dotenv/dotenv-environment.ts
@@ -1,15 +1,15 @@
 import { CheckEnvironment } from '@/data/check-environment.protocol';
-import { LoadEnvironments } from '@/data/load-environments.protocol';
+import { LoadEnvironmentsRepository } from '@/data/load-environments.repository';
 import { config, DotenvConfigOutput } from 'dotenv';
 
-export class DotEnvEnvironment implements CheckEnvironment, LoadEnvironments {
+export class DotEnvEnvironment implements CheckEnvironment, LoadEnvironmentsRepository {
     private configOutput: DotenvConfigOutput;
 
     constructor() {
         this.configOutput = config();
     }
 
-    list(): LoadEnvironments.Result {
+    list(): LoadEnvironmentsRepository.Result {
         return this.configOutput.parsed;
     }
 
