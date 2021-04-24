2aaa23d77e97fff7cbb67b67ebfaac0a805d2dbc
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-24 17:00:21 -0300

    style: update colors and typograpy famuly

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,48 +1,18 @@
-export const theme = {
+import { createMuiTheme } from '@material-ui/core/styles';
+export const theme = createMuiTheme({
     palette: {
-        common: {
-            black: '#000',
-            white: '#fff',
-            type: 'light',
-        },
         primary: {
             light: '#F2F8F1',
             main: '#56A086',
             dark: '#3C7257',
-            contrastText: '#fff',
         },
-
         secondary: {
             light: '#0091B9',
             main: '#0091B9',
             dark: '#0091B9',
-            contrastText: '#fff',
-        },
-        error: {
-            light: '#e57373',
-            main: '#f44336',
-            dark: '#d32f2f',
-            contrastText: '#fff',
-        },
-        grey: {
-            50: '#fafafa',
-            100: '#f5f5f5',
-            200: '#eeeeee',
-            300: '#e0e0e0',
-            400: '#bdbdbd',
-            500: '#9e9e9e',
-            600: '#757575',
-            700: '#6B7280',
-            800: '#424242',
-            900: '#212121',
-            A100: '#d5d5d5',
-            A200: '#aaaaaa',
-            A400: '#303030',
-            A700: '#616161',
-        },
-        background: {
-            paper: '#fff',
-            default: '#fafafa',
         },
     },
-};
+    typography: {
+        fontFamily: "'Kanit', 'Roboto', 'Arial', sans-serif",
+    },
+});
