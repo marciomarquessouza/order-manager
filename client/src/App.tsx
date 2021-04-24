93adbb26a5c13f678f49fc23bc241f548c66370c
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-24 17:02:43 -0300

    chore: theme tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 function App() {
     return (
         <main className="m-12">
-            <Typography variant="h5" color="textSecondary" align="center">
+            <Typography variant="h5" color="secondary" align="center">
                 Order Manager Client
             </Typography>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
         </main>
     );
 }
