eb99aa0d8c682bbf8c9168f755b170769443bb29
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 17:37:51 -0300

    style: add Storybook story to DataPicker component

---
+++ client/src/components/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Story, Meta } from '@storybook/react';
+import { DatePicker } from './DatePicker.component';
+import { IDatePickerProps } from './DatePicker.props';
+
+export default {
+    title: 'Order-Manager/DatePicker',
+    component: DatePicker,
+} as Meta;
+
+const Template: Story<IDatePickerProps> = (props) => {
+    const [date, setDate] = useState(1554284950000);
+    return (
+        <div className="m-4">
+            <DatePicker value={date} onChange={setDate} label="Booking Date" />
+        </div>
+    );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+    value: 1554284950000,
+};
