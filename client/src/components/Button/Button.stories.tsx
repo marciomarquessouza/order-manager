9d5a0f0669ef54afa3efdaf5f49a661bd39bb54d
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 00:20:22 -0300

    feat: add button stories

---
+++ client/src/components/Button/Button.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Button } from './Button.component';
+import { IButtonProps } from './Button.props';
+
+export default {
+    title: 'Order-Manager/Button',
+    component: Button,
+} as Meta;
+
+const Template: Story<IButtonProps> = (props) => (
+    <div className="m-4">
+        <Button {...props}>Button</Button>
+    </div>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+    color: 'primary',
+    variant: 'contained',
+};
+
+export const Text = Template.bind({});
+Text.args = {
+    color: 'secondary',
+    variant: 'text',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    color: 'primary',
+    variant: 'contained',
+    disabled: true,
+};
