df626771b99b1e4604b63907f27bd4a74eed6bb2
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 17:39:38 -0300

    feat: add props interface to DataPicker component

---
+++ client/src/components/DatePicker/DatePicker.props.ts
@@ -0,0 +1,7 @@
+import { ITests } from '../../tests/tests.interface';
+
+export interface IDatePickerProps extends ITests {
+    value: number;
+    onChange: (date: number) => void;
+    label: string;
+}
