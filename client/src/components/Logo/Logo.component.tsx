0fda6328e9a483f150098fa085ad906c0c51f596
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 20:13:43 -0300

    feat: became the component Logo in button to allows the onClick method

--- client/src/components/Logo/Logo.component.tsx
+++ client/src/components/Logo/Logo.component.tsx
@@ -3,6 +3,10 @@ import logo from '../../assets/logo.svg';
 import logoHeader from '../../assets/logo-header.svg';
 import { ILogoProps } from './Logo.props';
 
-export function Logo({ variant }: ILogoProps) {
-    return <img src={variant === 'standard' ? logo : logoHeader} alt="logo" />;
+export function Logo({ variant, onClick, testId = 'logo-component' }: ILogoProps) {
+    return (
+        <button data-testid={testId} onClick={onClick}>
+            <img src={variant === 'standard' ? logo : logoHeader} alt="logo" />
+        </button>
+    );
 }
