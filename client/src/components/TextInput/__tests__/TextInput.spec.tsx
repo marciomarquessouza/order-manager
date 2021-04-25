f433bf864f2c166ec568c8dd288984bb26963b9d
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-24 21:02:23 -0300

    test: ensure that it will call de onChange method

--- client/src/components/TextInput/__tests__/TextInput.spec.tsx
+++ client/src/components/TextInput/__tests__/TextInput.spec.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { TextInput, ITextInputProps } from '..';
-import { render } from '@testing-library/react';
-
-const setup = (props: ITextInputProps) => {
-    return render(<TextInput {...props} />);
-};
+import { render, fireEvent } from '@testing-library/react';
 
 const defaultProps: ITextInputProps = {
     value: 'test',
     onChange: jest.fn(),
 };
 
+const setup = (props: ITextInputProps = defaultProps) => {
+    const onChangeSpy = props.onChange;
+    const utils = render(<TextInput {...props} />);
+    const textInput = utils.getByTestId(/text-input-component/);
+    return {
+        textInput,
+        onChangeSpy,
+        ...utils,
+    };
+};
+
 describe('#TextInput', () => {
     it('renders component properly', () => {
-        const { getByTestId } = setup(defaultProps);
-        const component = getByTestId(/text-input-component/);
-        expect(component).toBeInTheDocument();
+        const { textInput } = setup();
+        expect(textInput).toBeInTheDocument();
+    });
+
+    describe('when a new text is entered', () => {
+        it('calls the onChange mehotd', () => {
+            const { textInput, onChangeSpy } = setup();
+            fireEvent.change(textInput, { target: { value: 'new text' } });
+            expect(onChangeSpy).toBeCalledWith('new text');
+        });
     });
 });
