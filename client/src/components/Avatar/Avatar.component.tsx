68f1476232593c54e368e6ec5f59c8280f2ff0a6
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 12:40:14 -0300

    style: add style to Avatar component

--- client/src/components/Avatar/Avatar.component.tsx
+++ client/src/components/Avatar/Avatar.component.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { IAvatarProps } from './Avatar.props';
+import { IAvatarProps, useStyles } from '.';
 import MaterialAvatar from '@material-ui/core/Avatar';
 
 export function Avatar({ name, email, onClick, testId = 'avatar-component' }: IAvatarProps) {
+    const classess = useStyles();
+
     return (
-        <div data-testid={testId} onClick={onClick} onKeyPress={onClick} role="button" tabIndex={0}>
-            <section>
-                <p>{name}</p>
-                <p>{email}</p>
+        <div
+            data-testid={testId}
+            onClick={onClick}
+            onKeyPress={onClick}
+            role="button"
+            tabIndex={0}
+            className="inline-flex"
+        >
+            <section className="flex-1 text-right pr-2 max-w-xs">
+                <p className="text-sm truncate">{name}</p>
+                <p className="text-xs text-gray-400 truncate">{email}</p>
             </section>
-            <aside>
-                <MaterialAvatar>{name.substr(0, 1)}</MaterialAvatar>
+            <aside className="flex-1 max-w-xs">
+                <MaterialAvatar className={classess.avatarStyle}>
+                    {name.substr(0, 1)}
+                </MaterialAvatar>
             </aside>
         </div>
     );
