c5e5cd7090b3edd5e717176a9d4fb98a205c2add
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-26 00:28:48 -0300

    style: add SearchInput story to StoryBook

---
+++ client/src/components/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { SearchInput } from './SearchInput.component';
+import { ISearchInputProps } from './SearchInput.props';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+export default {
+    title: 'Order-Manager/Components/SearchInput',
+    component: SearchInput,
+} as Meta;
+
+const Template: Story<ISearchInputProps> = (props) => {
+    return (
+        <div
+            style={{
+                background: 'linear-gradient(89.97deg, #3C7257 0.65%, #56A086 99.97%), #C4C4C4',
+                padding: 12,
+                width: '50%',
+            }}
+        >
+            <SearchInput {...props} />
+        </div>
+    );
+};
+
+export const Default = Template.bind({});
