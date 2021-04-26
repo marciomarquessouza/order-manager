4e8978f59dd9af9c1162c522a0f095984bd29139
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 23:22:16 -0300

    refactor: moving table data from storybook file

--- client/src/components/Table/Table.stories.tsx
+++ client/src/components/Table/Table.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import { Table, ITableProps, Column, Row } from './';
-import moment from 'moment';
+import { Table, ITableProps } from './';
+import { createTableProps } from './mock/Table.mock';
 
 export default {
     title: 'Order-Manager/Components/Table',
@@ -10,55 +10,5 @@ export default {
 
 const Template: Story<ITableProps> = (props) => <Table {...props} />;
 
-const columns: Column[] = [
-    {
-        id: 'title',
-        label: 'Title',
-        align: 'left',
-        minWidth: 170,
-    },
-    {
-        id: 'booking_date',
-        label: 'Booking Date',
-        align: 'left',
-        minWidth: 170,
-    },
-    {
-        id: 'address',
-        label: 'Address',
-        align: 'left',
-        minWidth: 170,
-    },
-    {
-        id: 'customer',
-        label: 'Customer',
-        align: 'left',
-        minWidth: 170,
-    },
-];
-
-interface Data {
-    code: string;
-    title: string;
-    booking_date: number;
-    address: string;
-    customer: string;
-}
-
-function createData(props: Data): Row {
-    const formatedDate = moment(1561172400000).format('DD.MM.YYYY');
-    return { ...props, booking_date: formatedDate };
-}
-
-const rows: Row[] = [
-    createData({
-        code: 'hKlIKPoZc2xCKGTUKZK2',
-        title: 'Test Order 1',
-        booking_date: 1561172400000,
-        address: 'Wriezener Str. 12',
-        customer: 'Emad Alam',
-    }),
-];
-
 export const Default = Template.bind({});
-Default.args = { columns, rows, onClick: () => undefined };
+Default.args = { ...createTableProps(), onClick: () => undefined };
