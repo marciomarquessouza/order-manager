0153ba52babf498872fe8ea6293e6200f14a3fc8
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-26 18:31:40 -0300

    style: page header styles

---
+++ client/src/layout/PageHeader/PageHeader.styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) => {
+    return createStyles({
+        headerContainer: {
+            background: `linear-gradient(89.97deg, ${theme.palette.primary.dark} 0.65%, ${theme.palette.primary.main} 99.97%), #C4C4C4`,
+            height: 175,
+            display: 'grid',
+            gridTemplateColumns: '25% 50% 25%',
+            gridTemplateRows: 'auto',
+        },
+    });
+});
