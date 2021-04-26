d8478d69ad512a7b4e170c1fe206d45c145d4087
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-25 22:13:30 -0300

    feat: add Table Component

---
+++ client/src/components/Table/Table.component.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import MaterialTable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import { ITableProps } from './Table.props';
+import { useStyles } from './Table.styles';
+
+export function Table({ rows, columns, onClick, testId = 'table-component' }: ITableProps) {
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [page, setPage] = React.useState(0);
+    const classes = useStyles();
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    const handleOnClick = (
+        event: React.MouseEvent<HTMLTableRowElement, MouseEvent>,
+        code: string,
+    ) => {
+        event.preventDefault();
+        onClick(code);
+    };
+
+    return (
+        <div>
+            <TableContainer>
+                <MaterialTable stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    className={classes.tableHeadStyle}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                    <TableRow
+                                        hover
+                                        role="checkbox"
+                                        tabIndex={-1}
+                                        key={`${row.code}`}
+                                        onClick={(event) => handleOnClick(event, `${row.code}`)}
+                                    >
+                                        {columns.map((column) => {
+                                            const rowValue = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {rowValue}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                    </TableBody>
+                </MaterialTable>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </div>
+    );
+}
