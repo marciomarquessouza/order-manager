422719f27aab7be181dffd37cb990c1a35bf58fc
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-27 20:12:23 -0300

    feat: wip authenticated flow

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { firebaseApp } from '../services';
 import { Auth } from '../pages/auth/Auth.config';
 import { Order } from '../pages/orders/Order.config';
 import { getUser } from '../pages/auth/SignIn.slice';
-import { useAppDispatch } from '../hooks';
+import { useAppDispatch, useAppSelector } from '../hooks';
 
 export function AppRouter() {
     const [isLoggedIn, setIsLoggedIn] = React.useState(false);
     const dispatch = useAppDispatch();
+    const { user, appState } = useAppSelector((state) => state.auth);
 
-    firebaseApp.auth().onAuthStateChanged((user) => {
-        if (user) {
-            dispatch(getUser());
+    firebaseApp.auth().onAuthStateChanged((currentUser) => {
+        if (currentUser) {
+            if (!user.name && appState === 'idle') {
+                dispatch(getUser());
+            }
             return setIsLoggedIn(true);
         }
         return setIsLoggedIn(false);
