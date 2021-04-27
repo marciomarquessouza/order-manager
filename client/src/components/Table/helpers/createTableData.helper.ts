355b4335264edc37c661110256a8038db2f0d2ea
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-26 22:53:37 -0300

    feat: add create Order List to table helper

--- client/src/components/Table/helpers/createTableData.helper.ts
+++ client/src/components/Table/helpers/createTableData.helper.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import { Row } from '../';
+import { IOrder } from '../../../entities/IOrder';
 
 interface Data {
     code: string;
@@ -12,4 +13,14 @@ interface Data {
 export function createOrderData(props: Data): Row {
     const formatedDate = moment(props.bookingDate).format('DD.MM.YYYY');
     return { ...props, bookingDate: formatedDate };
+}
+
+export function createOrderList(props: IOrder): Row {
+    return {
+        code: props.uid,
+        title: props.title,
+        address: props.address.street,
+        customer: props.customer.name,
+        bookingDate: moment(props.bookingDate).format('DD.MM.YYYY'),
+    };
 }
