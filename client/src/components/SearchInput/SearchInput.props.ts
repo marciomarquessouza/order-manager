1dfb5d4fda4650006a4fdc408aa6bd6f2277c5fb
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-27 20:06:31 -0300

    feat: enable send search input placeholder by props

--- client/src/components/SearchInput/SearchInput.props.ts
+++ client/src/components/SearchInput/SearchInput.props.ts
@@ -3,4 +3,5 @@ import { ITests } from '../../tests/tests.interface';
 export interface ISearchInputProps extends ITests {
     value: string;
     onChange: (text: string) => void;
+    placeholder?: string;
 }
