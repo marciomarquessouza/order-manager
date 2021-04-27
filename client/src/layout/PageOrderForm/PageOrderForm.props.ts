453ccd3edc1ba44e82c054b1a1640126cb7d23b5
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-27 01:01:58 -0300

    feat: add Page Order Form props interface

---
+++ client/src/layout/PageOrderForm/PageOrderForm.props.ts
@@ -0,0 +1,10 @@
+import { IOrder } from '../../entities/IOrder';
+
+export type IOrderProps = Omit<IOrder, 'uid'>;
+
+export interface IPageOrderFormProps {
+    order: IOrderProps;
+    onOrderChange: (order: IOrderProps) => void;
+    onSubmit: (order: IOrderProps) => void;
+    onOrderTitleChange: (title: string) => void;
+}
