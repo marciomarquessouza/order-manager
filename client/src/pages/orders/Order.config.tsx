97ca95e163d22b90600b71a1e9821ff6e273961d
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-28 02:18:27 -0300

    feat: add Order Detail

--- client/src/pages/orders/Order.config.tsx
+++ client/src/pages/orders/Order.config.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import { signOut } from '../../services';
 import { PageNav } from '../../layout/PageNav';
 import { useAppSelector } from '../../hooks';
 import { OrderList } from './OrderList.page';
 import { OrderForm } from './OrderForm.page';
+import { OrderDetail } from './OrderDetail.page';
 import { ORDER_CREATE, ORDER_DETAIL, ORDER_LIST } from '../../routes';
 
 export function Order() {
     const { user } = useAppSelector((state) => state.auth);
+    const history = useHistory();
+
+    const handleClickLogo = useCallback(() => {
+        history.push(ORDER_LIST);
+    }, [history]);
 
     return (
         <>
@@ -16,11 +22,11 @@ export function Order() {
                 email={user.email}
                 name={user.name}
                 onClickAvatar={signOut}
-                onClickLogo={() => undefined}
+                onClickLogo={handleClickLogo}
             />
             <Switch>
                 <Route path={ORDER_DETAIL}>
-                    <div>Order Detail</div>
+                    <OrderDetail />
                 </Route>
                 <Route path={ORDER_CREATE}>
                     <OrderForm />
