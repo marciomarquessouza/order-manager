57b7b60d40529e46ae5bda155800ea138f4aa71b
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-28 01:01:50 -0300

    feat: add return to order list

--- client/src/layout/PageCard/PageCard.component.tsx
+++ client/src/layout/PageCard/PageCard.component.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Card, CardContent, CardHeader, Paper, Typography } from '@material-ui/core';
+import React, { useCallback } from 'react';
+import { Card, CardContent, CardHeader, Paper } from '@material-ui/core';
 import { IPageCardProps } from './PageCard.props';
 import { useStyles } from './PageCar.styles';
+import { Button } from '../../components/Button';
+import { ArrowBack } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import { ORDER_LIST } from '../../routes';
 
 export function PageCard({ title, children }: IPageCardProps) {
     const classes = useStyles();
+    const history = useHistory();
+
+    const handleReturn = useCallback(() => {
+        history.push(ORDER_LIST);
+    }, [history]);
 
     return (
         <Paper>
@@ -12,6 +21,14 @@ export function PageCard({ title, children }: IPageCardProps) {
                 <CardHeader
                     className="bg-green-50"
                     title={<p className={classes.pageHeaderStyle}>{title}</p>}
+                    action={
+                        <div>
+                            <ArrowBack fontSize="small" color="primary" />
+                            <Button variant="text" onClick={handleReturn}>
+                                RETURN
+                            </Button>
+                        </div>
+                    }
                 />
                 <CardContent>{children}</CardContent>
             </Card>
