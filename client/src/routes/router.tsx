50942b4bceed9f33e94fe8a226dcad4295020dfc
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-28 03:00:43 -0300

    fix: add load between login status

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { firebaseApp } from '../services';
 import { Auth } from '../pages/auth/Auth.config';
 import { Order } from '../pages/orders/Order.config';
-import { getUser } from '../pages/auth/SignIn.slice';
-import { useAppDispatch, useAppSelector } from '../hooks';
+import { addUser } from '../pages/auth/SignIn.slice';
+import { useAppDispatch } from '../hooks';
+import { CircularProgress, Typography } from '@material-ui/core';
 
 export function AppRouter() {
-    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+    const [loginStatus, setLoginStatus] = React.useState<'idle' | 'signin' | 'signout'>('idle');
     const dispatch = useAppDispatch();
-    const { user, appState } = useAppSelector((state) => state.auth);
 
-    firebaseApp.auth().onAuthStateChanged((currentUser) => {
-        if (currentUser) {
-            if (!user.name && appState === 'idle') {
-                dispatch(getUser());
+    useEffect(() => {
+        firebaseApp.auth().onAuthStateChanged((currentUser) => {
+            if (currentUser) {
+                dispatch(
+                    addUser({
+                        name: currentUser.displayName || 'User',
+                        email: currentUser.email || '',
+                    }),
+                );
             }
-            return setIsLoggedIn(true);
-        }
-        return setIsLoggedIn(false);
-    });
+            setLoginStatus(currentUser ? 'signin' : 'signout');
+        });
+    }, [setLoginStatus, dispatch]);
 
-    return <Router>{!isLoggedIn ? <Auth /> : <Order />}</Router>;
+    if (loginStatus === 'idle') {
+        return (
+            <section className="flex items-center justify-center m-20">
+                <div className="m-4">
+                    <Typography variant="h5" color="secondary">
+                        Loading Data
+                    </Typography>
+                </div>
+                <CircularProgress color="secondary" size={28} />
+            </section>
+        );
+    }
+
+    return <Router>{loginStatus === 'signin' ? <Order /> : <Auth />}</Router>;
 }
