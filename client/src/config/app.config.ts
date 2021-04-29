73cc5f6d64726b7ccc763a7b5778e475e2898bd3
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-29 16:25:50 -0300

    chore: update config file with backend variable

--- client/src/config/app.config.ts
+++ client/src/config/app.config.ts
@@ -1,3 +1,3 @@
 export const ALERT_AUTO_HIDE = 6000;
 // eslint-disable-next-line no-undef
-export const BACKEND_SERVER_URL = process.env.API_URL || 'http://localhost:5000';
+export const BACKEND_SERVER_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
