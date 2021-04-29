43807d74868ff937ceddfec9c8b7e526bf1aa64e
marciomarquessouza <marciomultimedia@gmail.com>
2021-04-29 15:46:03 -0300

    chore: add environment variables to github page

---
+++ client/src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+export const FIREBASE_CONFIG = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
