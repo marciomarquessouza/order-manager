d9e6ff0d65a616a1bc89e6ac5888243f9362e8e3
marciomarquessouza <marciomultimedia@gmail.com>
2021-05-06 17:13:06 -0300

    feat: change title search by global search

--- client/src/pages/orders/Order.selectors.ts
+++ client/src/pages/orders/Order.selectors.ts
@@ -1,16 +1,24 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
 import { selectOrders } from './Order.slice';
+import moment from 'moment';
 
 export const filteredOrders = createSelector(
     (state: RootState) => state,
     (state) => {
-        const { searchText } = state.orders;
+        const searchText = state.orders.searchText.trim();
         if (!searchText) {
             return selectOrders(state);
         }
-        return selectOrders(state).filter((order) =>
-            order.title.toUpperCase().includes(searchText.toUpperCase()),
-        );
+        return selectOrders(state).filter((order) => {
+            const { title, bookingDate, address, customer } = order;
+            const columns = [
+                title,
+                customer.name,
+                address.street,
+                moment(bookingDate).format('DD.MM.YYYY'),
+            ];
+            return columns.some((text) => text.toLowerCase().includes(searchText.toLowerCase()));
+        });
     },
 );
